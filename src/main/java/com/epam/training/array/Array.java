041778644dd7f7c0aa5f25fbe2ec5980523b2b74
Edgaraszs <edgas258@gmail.com>
2021-04-05 22:23:59 +0300

    arrays

--- src/main/java/com/epam/training/array/Array.java
+++ src/main/java/com/epam/training/array/Array.java
@@ -1,8 +1,38 @@
 package com.epam.training.array;
 
+import java.util.Arrays;
+
 //TODO: write a wrapper class for a java int array, make this class immutable
 public class Array {
-  
-  private int[] items;
+
+  private final int[] items;
+
+  public Array(int[] items) {
+    this.items = items;
+  }
+
+  public int[] getItems() {
+    return items;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Array array = (Array) o;
+    return Arrays.equals(items, array.items);
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(items);
+  }
+
+  @Override
+  public String toString() {
+    return "Array{" +
+            "items=" + Arrays.toString(items) +
+            '}';
+  }
 
 }
